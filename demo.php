b199981bbc8897486da9ddad3c381b549570c180
samyn <zhangxi@vip.sina.com>
2018-02-04 15:41:30 +0800

    Add coment

--- demo.php
+++ demo.php
@@ -1,7 +1,7 @@
 <?php 
     $kkapi = new Apio;
     // 初始化接口，在调用业务接口前必须完成初始化，建议密钥口令在执行时录入，避免写入程序
-    // passphrase             : 在本地生成私钥时采用的口令
+    // passphrase             : 在本地生成私钥时采用的口令，例如：KKCOIN.COM
     // api_key                : 用户从 KKCOIN.COM 获取的字符串，例如：1f614ce13a2bebab9c6d24ead4f5ec8f
     // filepath_to_private_key: 保存私钥文件的路径 例如：file:///usr/local/var/yourprivate.key
     $kkapi->init('KKCOIN.COM','1f614ce13a2bebab9c6d24ead4f5ec8f','file:///usr/local/var/yourprivate.key');
