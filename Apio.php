08da3c0bccd83782a2f0b927fdabccfea759b0d1
Zhang Xi <zhangxi@vip.sina.com>
2018-02-10 15:44:32 +0800

    compact

---
+++ Apio.php
@@ -0,0 +1,139 @@
+<?php
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// 命令行入口
+/////////////////////////////////////////////////////////////////////////////////////////////////
+$Apio = new ApioController;
+$method = $argv[1];
+$params = array_slice($argv,2);
+if (!empty($params)){
+    $Apio->$method($params);
+}else{
+    $Apio->$method();
+}
+
+class ApioController {
+    /////////////////  CUSTOMIZE /////////////////
+    const API_PRIVATE_KEY = '/yourprivate.key';
+    const API_PUBLIC_KEY = '/yourpublic.key';
+    const API_KEY_PASSPHRASE = 'KKCOIN.COM';
+    const API_KEY = 'place your key';
+    //const API_KEY = 'place your key';
+    const API_REST_URL = 'https://api.kkcoin.com/rest/';
+    //const API_REST_URL = 'http://localhost:8081/api/';
+    ////////////////////////////////////////////////
+
+
+    const API_HEADER_KEY  = 'kkcoinapikey: ';
+    const API_HEADER_SIGN = 'kkcoinsign: ';
+    const API_HEADER_TIME = 'kkcointimestamp: ';
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// REST API 统一调用接口
+/////////////////////////////////////////////////////////////////////////////////////////////////
+    private function rest($endpoint, $payload = array(), $get = true){
+        $path = getcwd();
+        $timestamp = time();
+        $private_key_file = $path.self::API_PRIVATE_KEY;  
+        $public_key_file = $path.self::API_PUBLIC_KEY;
+        ksort($payload, SORT_STRING | SORT_FLAG_CASE);
+        $message = $endpoint.json_encode($payload).$timestamp;
+        $method = $get?'GET':'POST';
+
+        //self::dump('私钥文件：'.$private_key_file);   
+        //self::dump('公钥文件: '.$public_key_file);     
+        //self::dump('私钥口令: '.self::API_KEY_PASSPHRASE);   
+        self::dump('路由节点：'.$endpoint);
+        self::dump('访问方式：'.$method);
+        self::dump('时 间 戳：'.$timestamp);
+        self::dump('参数数组：'.json_encode($payload));
+        self::dump('消息报文：'.$message);
+
+        $openssl_res = openssl_get_privatekey('file://'.$private_key_file, self::API_KEY_PASSPHRASE);
+        $ssl_res = openssl_sign($message, $signature_bin, $openssl_res, OPENSSL_ALGO_SHA256);
+        openssl_free_key($openssl_res);
+        $signature = base64_encode($signature_bin);
+        self::dump('Base64后的签名：\n'.$signature);
+        $header_array = array(
+            self::API_HEADER_KEY.self::API_KEY,
+            self::API_HEADER_SIGN.$signature,
+            self::API_HEADER_TIME.$timestamp);
+        // 输出
+        $http_res = curl_init();
+        if ($get){
+            if (empty($payload)){
+                $http_url =  self::API_REST_URL.$endpoint;
+            }else{
+                $http_url =  self::API_REST_URL.$endpoint.'?'.http_build_query($payload);
+            }
+        }else{
+            $http_url =  self::API_REST_URL.$endpoint;
+            curl_setopt($http_res, CURLOPT_POST, 1);
+            curl_setopt($http_res, CURLOPT_POSTFIELDS, $payload);
+        }
+        self::dump('开始访问：'.$http_url);
+        //self::dump('组装好的定制HEADER数组：\n'.json_encode($header_array));
+        curl_setopt($http_res, CURLOPT_URL, $http_url); 
+        curl_setopt($http_res, CURLOPT_RETURNTRANSFER, 1); 
+        curl_setopt($http_res, CURLOPT_HTTPHEADER, $header_array);
+        $response = curl_exec($http_res);
+        curl_close($http_res);
+        echo "服务器返回：\n";
+        return $response;
+    }
+
+    function dump($exp)
+    {
+        print_r($exp);
+        print_r("\n"); 
+    }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// 业务入口
+/////////////////////////////////////////////////////////////////////////////////////////////////
+    // 获取用户账户余额
+    public function balance(){
+        $response = self::rest(__FUNCTION__);
+        var_dump($response);
+        $arr = json_decode($response,true);
+        if (is_null($arr)){
+            print_r($response);
+        }else{
+            $bal = array();
+            foreach ($arr as $key => $value) {
+                if (intval($value['bal']) > 0){
+                    $bal[] = $value;
+                }
+            }
+            var_dump($bal);            
+        }
+    }
+    public function order($params = array('20180207000000000147')){
+        $payload = array('id' => $params[0]);
+        $response = self::rest(__FUNCTION__,$payload);
+        $arr = json_decode($response,true);
+        var_dump($arr);
+    }
+    public function openorders($params = array("EOS_ETH")){
+        $payload = array('symbol' => $params[0]);
+        $response = self::rest(__FUNCTION__,$payload);
+        $arr = json_decode($response,true);
+        var_dump($arr);
+    }
+    public function trade($params = 
+        array('KK_ETH', 'LIMIT', 'BUY', '0.01', '1000')){
+        $payload['symbol'] = $params[0];
+        $payload['ordertype'] = $params[1];        
+        $payload['orderop'] = $params[2];            
+        $payload['price'] =  $params[3];               
+        $payload['amount']=  $params[4];       
+        $response = self::rest(__FUNCTION__, $payload, false);
+        $arr = json_decode($response,true);
+        var_dump($arr);
+    }
+    public function cancel($params = array('20180207000000000147')){
+        $payload = array('id' => $params[0]);
+        $response = self::rest(__FUNCTION__, $payload, false);
+        $arr = json_decode($response,true);
+        var_dump($arr);
+    }
+}
---
+++ ApioController.php
@@ -0,0 +1,99 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+
+class ApioController extends Controller{
+    // 获取用户账户余额
+    public function balance(Request $request){
+        $this->rest(__FUNCTION__);
+    }
+    public function order(Request $request){
+        $id = 111;
+        $payload = array('id' => (string)$id);
+        $this->rest(__FUNCTION__,$payload);
+    }
+    public function openorders(Request $request){
+        $payload = array('symbol' => "EOS_ETH");
+        $this->rest(__FUNCTION__,$payload);
+    }
+    public function trade(Request $request){
+        $payload['symbol'] = "EOS_ETH";
+        $payload['ordertype'] = 'LIMIT';        //1-LIMIT 2-MARKET 3-STOP 4-STOP_LIMIT
+        $payload['orderop'] = 'BUY';            //1-BUY 2-SELL
+        $payload['price'] = 0.01;               //价格
+        $payload['amount']= 10;                 //数量
+
+        $payload['price'] = (string)$payload['price'];
+        $payload['amount'] = (string)$payload['amount'];
+        $this->rest(__FUNCTION__, $payload, false);
+    }
+    public function cancel(Request $request){
+        $orderid = '1234234234234';
+        $payload = array();
+        $payload = array('id' => (string)$orderid);
+        $this->rest(__FUNCTION__, $payload, false);
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+    // REST API 统一调用接口
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+    private function rest($endpoint, $payload = array(), $get = true){
+        $passphrase = 'KKCOIN.COM';
+        $path = storage_path();
+        $private_key_file = 'file://'.$path.'/yourprivate.key'; 
+        dump('私钥文件：'.$private_key_file);        
+        dump('私钥口令: '.$passphrase); 
+        $public_key_file = 'file://'.$path.'/yourpublic.key'; 
+        dump('公钥文件: '.$public_key_file);     
+        $contents = File::get($path.'/yourpublic.key');
+        print_r("<code>$contents</code>");
+        $api_url = 'http://localhost:8081/api/';
+        $header_api_key = 'kkcoinapikey: 1f614ce13a2bebab9c6d24ead4f5ec8f';
+        $header_sign = 'kkcoinsign: ';
+        $header_timestamp = 'kkcointimestamp: ';
+        // 签名 & 组装
+        $timestamp = time();
+        if ($get){
+            dump('路由：'.$endpoint.' 访问方式： GET   时间戳：'.$timestamp);
+        }else
+        {
+            dump('路由：'.$endpoint.' 访问方式： POST  时间戳：'.$timestamp);          
+        }
+        dump('参数数组：'.json_encode($payload));
+        ksort($payload, SORT_STRING | SORT_FLAG_CASE);
+        $message = $endpoint.json_encode($payload).$timestamp;
+        dump('组装好的消息报文：'.$message);
+        $openssl_res = openssl_get_privatekey($private_key_file, $passphrase);
+        $ssl_res = openssl_sign($message, $signature_bin, $openssl_res, OPENSSL_ALGO_SHA256);
+        openssl_free_key($openssl_res);
+        $signature = base64_encode($signature_bin);
+        dump('Base64后的签名：'.$signature);
+        $header_array = array(
+            $header_api_key,
+            $header_sign.$signature,
+            $header_timestamp.$timestamp);
+        // 输出
+        $http_res = curl_init();
+        if ($get){
+            $http_url =  $api_url.$endpoint.'?'.http_build_query($payload);
+        }else{
+            $http_url =  $api_url.$endpoint;
+            curl_setopt($http_res, CURLOPT_POST, 1);
+            curl_setopt($http_res, CURLOPT_POSTFIELDS, $payload);
+        }
+        dump('组装好的访问地址：'.$http_url);
+        dump('组装好的定制HEADER数组：'.json_encode($header_array));
+        curl_setopt($http_res, CURLOPT_URL, $http_url); 
+        curl_setopt($http_res, CURLOPT_RETURNTRANSFER, 1); 
+        curl_setopt($http_res, CURLOPT_HTTPHEADER, $header_array);
+        $response = curl_exec($http_res);
+        curl_close($http_res);
+        dump('服务器返回：'.$response);
+    }
+
+    function dump($exp)
+    {
+        print "<BR>"; var_dump($exp); print "<BR>"; 
+    }
+}
