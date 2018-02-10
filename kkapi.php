bbb5add65a8a8da2f3fd85c256e989791bec8dd1
Zhang Xi <zhangxi@vip.sina.com>
2018-02-10 17:25:39 +0800

    require

--- kkapi.php
+++ kkapi.php
@@ -1,13 +1,5 @@
 <?php
-    /////////////////  CUSTOMIZE /////////////////
-    const API_PRIVATE_KEY = '/yourprivate.key';
-    const API_PUBLIC_KEY = '/yourpublic.key';
-    const API_KEY_PASSPHRASE = 'KKCOIN.COM';
-    const API_KEY = 'place your key';
-    //const API_KEY = 'place your key';
-    const API_REST_URL = 'https://api.kkcoin.com/rest/';
-    //const API_REST_URL = 'http://localhost:8081/api/';
-    ////////////////////////////////////////////////
+require('customize.php');
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // 命令行入口
